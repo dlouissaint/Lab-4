e4a2bd041afd50f0ec71e798dd8a9732fa7e9160
dlouissaint <32205806+dlouissaint@users.noreply.github.com>
2017-09-29 15:42:53 -0400

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main (String [] args) {
+        int wage = 20;
+
+        System.out.print("Salary is ");
+        System.out.println(wage * 40 * 50);
+
+        return;
+    }
+}
+
